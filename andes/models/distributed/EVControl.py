99fae0382999479a379acb5ed756f0326d5b20bf
treymingee <vxl191@vols.utk.edu>
2021-11-15 11:48:56 -0500

    EVControl v1

---
+++ andes/models/distributed/EVControl.py
@@ -0,0 +1,91 @@
+"""Electric vehicle charging rate control"""
+
+from andes.core.param import NumParam
+from andes.core.var import Algeb
+from andes.core.discrete import Limiter, DeadBand
+
+from andes.models.distributed.ev import EV2Data, EV2Model
+
+
+class EVControlData(EV2Data):
+    """
+    Data for EV charging rate control
+    """
+    def __init__(self):
+        EV2Data.__init__(self)
+        self.chargerv = NumParam(default=400.0, info='Rated voltage of EV charger', #May be unnecessary to include?
+                            tex_name='v_{charger}',
+                            unit='V',
+                            )
+        #Replace Icmin and Icmax with power min/max.
+        self.Pcmin = NumParam(default=0.0, info='Minimum charging power',
+                            tex_name='P_{c,min}',
+                            unit='A',
+                            )
+        self.Pcmax = NumParam(default=69.0, info='Maximum charging current', #Will change depending on SOC of cluster
+                            tex_name='P_{c,max}',
+                            unit='A',
+                            )
+        self.fthreshold = NumParam(default=0.6, info='Frequency threshold for charge control to activate',
+                            tex_name='f_{threshold}',
+                            unit='Hz',
+                            )
+        self.NumEV = NumParam(default=100,
+                             info='Number of EVs connected to the system within the same SOC range',
+                             tex_name='NumEV',
+                             )                            
+        self.pratio = NumParam(default=-0.75,
+                             info='Initial charging power ratio multiplied to pmx in [-1, 1]',
+                             tex_name='p_{ratio}',
+                             vrange=(-1, 1),
+                             )
+        self.FDevMax = NumParam(default=5.0,
+                             info='Maximum frequency deviation from 60Hz that EVs will alter charging power to support',
+                             tex_name='freq_{dev,max}',
+                             unit='Hz',
+                             )
+        self.tsteady = NumParam(default=2.0, info='Length of time that frequency stays within threshold frequency to turn charge control off',
+                            tex_name='t_{steady}',
+                            unit='s',
+                            )
+
+class EVControlModel(EV2Model):
+    """
+    Model implementation of EV charge control
+    """
+    def __init__(self, system, config):
+        EV2Model.__init__(self, system, config)
+
+        #Set nominal charging power
+        self.Pcnom = Algeb(info='Nominal charging power',
+                           v_str='(Pcmax - Pcmin)*Pratio',
+                           e_str='(Pcmax - Pcmin)*Pratio-Pcnom',
+                           tex_name='P_{c,nom}'
+                           )
+
+        #Set total charging power of each cluster
+        self.Pcluster = Algeb(info='Charging power of each EV cluster',
+                           v_str='Pcnom*NumEV',
+                           e_str='Pcnom*NumEV - Pcluster',
+                           tex_name='P_{cluster}'
+                           )
+        
+        #If freqdev is larger than freqtheshold (0.6Hz for now), apply power change. Can use the deadband comparer?
+        self.freqDev = Algeb(info='COI Frequency deviation from 60Hz',
+                             v_str='COI - 60',
+                             e_str='COI - 60 - freqDev',
+                            )
+                            
+        self.freqDB = DeadBand(u=self.freqDev, center=60, lower=59.4,
+                            upper=60.6, tex_name='FreqDB',
+                            info='deadband for COI speed',
+                            )
+        
+        #Define charge control equation based on freqDB
+        self.Pnew = Algeb(info='Charging power after comparing COI frequency deviation to 60Hz',#Should this be self.Pe?
+                          v_str='(Pcluster + (freqDev/FDevMax)*(Pcmax*NumEV - Pcluster))*freqDB_zl + Pcluster*freqDB_zi + (Pcluster + (freqDev/FDevMax)*(Pcmax*NumEV - Pcluster))*freqDB_zu',
+                          e_str='(Pcluster + (freqDev/FDevMax)*(Pcmax*NumEV - Pcluster))*freqDB_zl + Pcluster*freqDB_zi + (Pcluster + (freqDev/FDevMax)*(Pcmax*NumEV - Pcluster))*freqDB_zu - Pnew'
+        )
+
+#TODO: Define charge control equation as function of COI freq. deviation outside threshold
+        
